4e8bae6600a6ae6fcd0131927040ce6cefce0163
Veggissss <vegarddem@gmail.com>
2026-02-17 11:13:51 +0100

    feat: added allowed host in vite.config with defined env vars in Dockerfile.

--- vite.config.ts
+++ vite.config.ts
@@ -11,22 +11,26 @@ export default defineConfig(({ mode }) => {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
     plugins: [react(), tailwindcss()],
+
     server: {
       port: env.APP_PORT ? Number(env.APP_PORT) : 4770,
       strictPort: true,
       host: true,
       origin: `http://0.0.0.0:${env.APP_PORT ? Number(env.APP_PORT) : 4770}`,
+
       proxy: {
         '/backend': {
-          target: 'https://solarwatch-api-csharp.onrender.com',
+          target: `${env.APP_BACKEND_API ? env.APP_BACKEND_API : "http://localhost:8080"}`,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/backend/, '')
         }
       }
     },
+
     preview: {
       port: env.APP_PORT ? Number(env.APP_PORT) : 4770,
       strictPort: true,
+      allowedHosts: env.APP_HOSTNAME ? [env.APP_HOSTNAME] : ["localhost"]
     },
   }
 })
