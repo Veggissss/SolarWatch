d8c702e8f4230d34a366bfd71262fd16619f0ba0
Veggissss <vegarddem@gmail.com>
2026-02-19 07:53:52 +0100

    fix: add base url path to react router.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,8 @@ async function enableMocking() {
 enableMocking().then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+
         <App />
       </BrowserRouter>
     </StrictMode>
